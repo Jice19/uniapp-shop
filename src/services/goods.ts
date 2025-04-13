648291100a9fb539ee6c651118e377f2907e0c0a
jice19 <15590761+jice19@user.noreply.gitee.com>
2025-04-13 13:20:26 +0800

    feat : 详情页轮播图数字绑定

--- src/services/goods.ts
+++ src/services/goods.ts
@@ -4,10 +4,11 @@
 // 可以使用
 // 规格集合一定要和skus集合下的specs 顺序保持一致
 
+import type { GoodsResult } from "@/types/goods"
 import { http } from "@/utils/http"
 
 export const getDataByIdApi = (id : string ) => {
-  return http({
+  return http<GoodsResult>({
     method:'GET',
     url:'/goods',
     data:{id}
